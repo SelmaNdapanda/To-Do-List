19399188c1cb9727d91a252f13cd829023097e1b
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-27 23:46:58 +0200

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
+};
