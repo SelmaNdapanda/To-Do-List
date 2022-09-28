ab15b4bffadbb236f15a284e5017120530fcf049
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-28 13:23:42 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
 const task = [
-    { description: 'Cook lunch', isCompleted: false, index: 0 },
-    { description: 'Do laundry', isCompleted: false, index: 1 },
-    { description: 'Cook dinner', isCompleted: false, index: 2 },
-  ];
-  
-  const taskList = () => {
-    let taskListContent = '';
-    task.forEach((item) => {
-      taskListContent += `<li class="activity"><input class="activity-check" type="checkbox"><span class="list">${item.description}</span></li>`;
-    });
-    document.querySelector('.activity-container').innerHTML = taskListContent;
-  };
+  { description: 'Cook lunch', isCompleted: false, index: 0 },
+  { description: 'Do laundry', isCompleted: false, index: 1 },
+  { description: 'Cook dinner', isCompleted: false, index: 2 },
+];
+
+const taskList = () => {
+  let taskListContent = '';
+  task.forEach((item) => {
+    taskListContent += `<li class="activity"><input class="activity-check" type="checkbox"><span class="list">${item.description}</span></li>`;
+  });
+  document.querySelector('.activity-container').innerHTML = taskListContent;
+};
 taskList();
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   module: {
