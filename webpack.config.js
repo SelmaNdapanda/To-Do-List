2df210025d1902f8e2d84651ba6b949b7e404ff9
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-28 11:28:06 +0200

    Set Up Webpack Development Server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
