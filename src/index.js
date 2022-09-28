f76be1fc5c4b5852c450e2ac1f342bce3bbf25ea
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-28 15:46:01 +0200

    Delete task array items

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 // import _ from 'lodash';
 import './style.css';
 
-const task = [
-  { description: 'Cook lunch', isCompleted: false, index: 0 },
-  { description: 'Do laundry', isCompleted: false, index: 1 },
-  { description: 'Cook dinner', isCompleted: false, index: 2 },
-];
-
 const taskList = () => {
   let taskListContent = '';
   task.forEach((item) => {
