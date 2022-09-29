ca5b2bd6d39d29a1335ec60fd938c639ac34529c
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-29 13:25:47 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// import _ from 'lodash';
 import './style.css';
 
+const task = [];
 const taskList = () => {
   let taskListContent = '';
   task.forEach((item) => {
-    taskListContent += `<li class="activity"><input class="activity-check" type="checkbox"><span class="list">${item.description}</span></li>`;
+    taskListContent += `<li class="task"><input class="task-check" type="checkbox"><span class="list">${item.description}</span><i class='fa fa-ellipsis-v'></i></li>`;
   });
   document.querySelector('.activity-container').innerHTML = taskListContent;
 };
