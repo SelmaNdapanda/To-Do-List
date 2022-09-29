8fa3673987232a5d5d341f0aa2feb6901a33a325
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-09-29 13:03:26 +0200

    Implement Delete, Update and Add functionalities

---
+++ src/task.js
@@ -0,0 +1,23 @@
+import { saveLocal } from './storage.js';
+
+export const add = (tasks) => {
+  console.log(document.querySelector('#description').value)
+  tasks.push({
+    description: document.querySelector('#description').value, isCompleted: false, index: tasks.length + 1,
+  });
+  saveLocal(tasks);
+};
+
+export const updateIndex = (tasks) => {
+  let i = 1;
+  tasks.forEach((elem) => {
+    elem.index = i;
+    i += 1;
+  });
+};
+
+export const removeDone = (tasks) => {
+  tasks = tasks.filter((elem) => elem.isCompleted === false);
+  updateIndex(tasks);
+  saveLocal(tasks);
+};
